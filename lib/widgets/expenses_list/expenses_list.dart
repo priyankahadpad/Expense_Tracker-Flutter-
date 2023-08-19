de5d14eaffc670e04ebf34edf3a5b1424f6366c7
Vitthal <vhadpad@gmail.com>
2023-08-19 13:55:30 +0530

    first commit

---
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -0,0 +1,33 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expenses_item.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({
+    super.key,
+    required this.expenses,
+    required this.onRemoveExpense,
+  });
+
+  final List<Expense> expenses;
+
+  final void Function(Expense expense) onRemoveExpense;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (ctx, index) => Dismissible(
+        onDismissed: (direction) => onRemoveExpense(expenses[index]),
+        key: ValueKey(expenses[index]),
+        background: Container(
+          color: Theme.of(context).colorScheme.error.withOpacity(0.50),
+          margin: EdgeInsets.symmetric(
+            horizontal: Theme.of(context).cardTheme.margin!.horizontal,
+          ),
+        ),
+        child: ExpenseItem(expenses[index]),
+      ),
+    );
+  }
+}
